0c5c6d7bca9a40a9de7f03782f09839eeec4fa32
Max Malm <benjick@dumfan.net>
2015-06-30 01:55:01 +0200

    clean up

--- package.js
+++ package.js
@@ -9,14 +9,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.export('webcam');
   api.addFiles([
     'tota11y/build/tota11y.min.js',
   ], 'client');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('benjick:tota11y');
-  api.addFiles('tota11y-tests.js');
 });
